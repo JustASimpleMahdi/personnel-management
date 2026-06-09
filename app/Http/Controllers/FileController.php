e52687eba48d7f697b91573f3725aa8777a7124d
JustASimpleMahdi <just.a.simple.mahdi@gmail.com>
2026-06-09 23:54:13 +0330

    bugfix show report files for manager

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\File;
 use App\Models\Report;
+use App\RoleEnum;
 use Storage;
 
 class FileController extends Controller
 {
-    public function reportFile(File $file){
-        if(!auth()->user()->reports->some(fn(Report $report) => $report->files()->where('file_id',$file->id)->exists()))
+    public function reportFile(File $file)
+    {
+        $user = auth()->user();
+        if (!($user->reports->some(fn(Report $report) => $report->files()->where('file_id', $file->id)->exists())
+            || $user->role->key === RoleEnum::MANAGER))
             abort(403);
 
         return Storage::disk($file->disk)->download($file->path, $file->filename);
