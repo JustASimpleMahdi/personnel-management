e7ef3750f487e51ef788b3e6321fbbee6f79d462
JustASimpleMahdi <just.a.simple.mahdi@gmail.com>
2026-06-10 00:01:49 +0330

    bugfix add multiple attachments to reports

--- resources/views/reports/edit.blade.php
+++ resources/views/reports/edit.blade.php
@@ -174,7 +174,7 @@
         @endforeach
         <label class="attachment-btn">
             <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21.44 11.05l-9.19 9.19a6 6 0 01-8.49-8.49l9.19-9.19a4 4 0 015.66 5.66l-9.2 9.19a2 2 0 01-2.83-2.83l8.49-8.48"></path></svg>
-            <input name="files[]" type="file">
+            <input name="files[]" multiple type="file">
         </label>
         @error('files[]')
         <div class="error-message">{{ $message }}</div>
